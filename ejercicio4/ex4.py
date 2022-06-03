03b567e5628b36fb90c05762b61437aa64b2a79c
Julian Sarrelli <julian.sarrelli@tiendanube.com>
2022-06-03 16:34:22 -0300

    ejercicio 4

---
+++ ejercicio4/ex4.py
@@ -0,0 +1,31 @@
+from Bio.Blast import NCBIXML
+import sys
+
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
+
+from Bio import Entrez
+
+
+def look_for_matches(pattern):
+    print(f"Searching for hit with pattern: {pattern}")
+    result_handle = open("./output/blast_result.xml")
+    blast_record = NCBIXML.read(result_handle)
+    for alignment in blast_record.alignments:
+        description = alignment.hit_def
+        if pattern in description:
+            print(f"Hit: {description} / id: {alignment.accession}")
+            download_records(alignment.hit_id, description)
+
+def download_records(accession_number, description):
+    Entrez.email = "julisarrelli@gmail.com"
+    handle = Entrez.efetch(db="protein", id=accession_number, rettype="gb", retmode="text")
+    record = SeqIO.read(handle, "genbank")
+    handle.close()
+    sequence = SeqRecord(seq=record.seq, id="NP_001375421.1", description=description)
+    SeqIO.write(sequence, f"./ejercicio4/{description}.fasta", "fasta")
+
+if __name__ == '__main__':
+    parameter_pattern = sys.argv[1]
+    look_for_matches(parameter_pattern)
---
+++ ejercicio5/ex5.py
@@ -0,0 +1,4 @@
+from Bio.Emboss.Applications import NeedleCommandline
+
+if __name__ == '__main__':
+    help(NeedleCommandline)
