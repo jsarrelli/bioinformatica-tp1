a24f268cc67e183ea1ecb2606ac10d8956f21f8b
Julian Sarrelli <julian.sarrelli@tiendanube.com>
2022-06-05 10:17:54 -0300

    5 first iteration

--- ejercicio5/ex5.py
+++ ejercicio5/ex5.py
@@ -1,4 +1,14 @@
-from Bio.Emboss.Applications import NeedleCommandline
+import os
+
+
+# https://www.ebi.ac.uk/Tools/st/emboss_backtranseq/
+# https://www.bioinformatics.nl/cgi-bin/emboss/help/backtranseq
+# backtranseq reads a protein sequence and writes the nucleic acid sequence it is most likely to have come from.
+
+def translate_protein_to_nucleic_sequence(input_file):
+    os.system(f'backtranseq {input_file} ../output/nucleic_sequence.fasta')
+
 
 if __name__ == '__main__':
-    help(NeedleCommandline)
+    # translate protein sequence (ORF 11 from NM_001388492.1) to nucleic sequence
+    translate_protein_to_nucleic_sequence("../output/HTT.fasta")
